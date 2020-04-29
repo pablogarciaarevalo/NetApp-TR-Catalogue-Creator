4ad585ca88ff90917cb7a51a837fc76b8366464e
Pablo Garcia Arevalo <pgarevalo@gmail.com>
2020-04-29 07:11:09 +0200

    Remove the content-type != application/pdf

--- TR-catalogue-creator.py
+++ TR-catalogue-creator.py
@@ -24,7 +24,7 @@ while first <= last:
     if first not in blacklist:
 
         # Requesting the document from the public website
-        varUrl = "http://www.netapp.com/us/media/tr-" + str(first) + ".pdf"
+        varUrl = "https://www.netapp.com/us/media/tr-" + str(first) + ".pdf"
         r = requests.get(varUrl)
 
         # Check if the document is already in the catalogue
@@ -36,31 +36,39 @@ while first <= last:
         # If the document is currently public
         if (r.status_code == 200):
 
-            # If it is not included in the catalogue
-            if len(matching) == 0:
-            
-                # Download the document
-                with open(str(first) + ".pdf", "wb") as pdf:
-                    pdf.write(r.content)
+            # Check if the requested http object is a PDF file
+            isPDFfile = (r.headers['content-type']=='application/pdf')
+            if isPDFfile:
+
+                # If it is not included in the catalogue
+                if len(matching) == 0:
                 
-                # Get the title from the PDF file
-                myPDF = open(str(first) + ".pdf", 'rb')
-                try:
-                    currentTitle = PdfReader(myPDF).Info.Title.strip('()')
-                except:
-                    pass
+                    # Download the document
+                    with open(str(first) + ".pdf", "wb") as pdf:
+                        pdf.write(r.content)
+                    
+                    # Get the title from the PDF file
+                    myPDF = open(str(first) + ".pdf", 'rb')
+                    try:
+                        currentTitle = PdfReader(myPDF).Info.Title.strip('()')
+                    except:
+                        pass
 
-                myPDF.close()
+                    myPDF.close()
 
-                # Remove the local document
-                os.remove(str(first) + '.pdf')
+                    # Remove the local document
+                    # os.remove(str(first) + '.pdf')
 
-            # If it is included in the catalogue
-            if len(matching) != 0:
-                
-                # Get the title form the catalogue
-                currentTitle = ''.join(matching)
+                # If it is included in the catalogue
+                if len(matching) != 0:
+                    
+                    # Get the title form the catalogue
+                    currentTitle = ''.join(matching)
 
-            print('<a href="http://www.netapp.com/us/media/tr-' + str(first) + '.pdf" class="TR-url" target="_blank">' + currentTitle + '</a><br />')
+                    # Download the document
+                    with open(str(first) + ".pdf", "wb") as pdf:
+                        pdf.write(r.content)
+
+                print('<a href="https://www.netapp.com/us/media/tr-' + str(first) + '.pdf" class="TR-url" target="_blank">' + currentTitle + '</a><br />')
 
     first = first + 1
